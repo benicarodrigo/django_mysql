4b463c3005953a8041ca24933236892418401fdf
Benica <benicarodrigo@gmail.com>
2021-01-27 14:19:56 -0300

    Prpjeto Finalizado

---
+++ core/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContatoForm, ProdutoModelForm
+from .models import Produto
+
+
+def index(requests):
+    context = {
+        'produtos': Produto.objects.all()
+
+    }
+    return render(requests, 'index.html', context)
+
+
+def contato(requests):
+    form = ContatoForm(requests.POST or None)
+    if str(requests.method) == 'POST':
+        if form.is_valid():
+            form.send_email()
+            messages.success(requests, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(requests, 'Erro ao enviar e-mail!')
+
+    context = {
+        'form': form
+    }
+    return render(requests, 'contato.html', context )
+
+
+def produto(requests):
+    if str(requests.user) != 'AnonymousUser':
+        if str(requests.method) == 'POST':
+            form = ProdutoModelForm(requests.POST, requests.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(requests, 'Produto Salvo com sucesso')
+                form = ProdutoModelForm()
+            else:
+                messages.error(requests, 'Erro ao salvar o produto.')
+        else:
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(requests, 'produto.html', context)
+    else:
+        return redirect('index')
+
+
+
+
